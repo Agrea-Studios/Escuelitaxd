eafee16b390694ebfffaf3fe6cfdb329a8027ab2
DresdenMZ <maurix212@gmail.com>
2021-01-18 21:10:41 -0600

    Radio de un círculo.
    Pues lo que dice el título. xD

--- Mauricio/Program.cs
+++ Mauricio/Program.cs
@@ -6,7 +6,16 @@ namespace Mauricio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingresa un valor:");
+            Double pi = 3.1416;
+            Double r;
+            Double r1;
+            Double rf;
+            r = Convert.ToDouble(Console.ReadLine());
+            r1 = r * pi;
+            rf = r1 * r1;
+            Console.WriteLine("El area es:"+ rf);
+
         }
     }
 }
