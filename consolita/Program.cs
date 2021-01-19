73f9f71c9ba11dba30c888dc723e388183e1996d
jorge <sr.conejo.uwu@gmail.com>
2021-01-18 18:34:48 -0600

    .
    .

---
+++ consolita/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace consolita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            /*   
+             *   int permite almacenar datos de tipo entero 
+             *   float nos permite almacenar datos con punto decimal 
+             *   bool nos permite almacenar datos de tipo (verdadero o falso)
+             *   char nos permite almacenar un caracter
+             *   string nos permite almacenar cadenas de texto 
+             *   var automatica 
+             */
+
+            // Operadores aritmeticos 
+            /*
+             * + Adicion o suma
+             * - Sustraccion o resta 
+             * / Division 
+             * * Multiplicacion
+             * % Resto 
+             * 
+             */
+
+            int x = 5;
+            int y = 2;
+            float z = 3.7f;
+
+            x = x + y;
+            x = x * 32;
+           
+
+            Console.WriteLine("el valor de x es");
+            Console.WriteLine(x);
+
+
+
+        }
+    }
+}
