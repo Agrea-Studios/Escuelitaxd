10a31307c99f93b15fca78ff22e39f1cced00bed
ElCarlos97 <66186531+ElCarlos97@users.noreply.github.com>
2021-01-19 19:07:36 -0600

    algo
    equisde

---
+++ segundo intento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace segundo_intento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.1416;
+
+            int radio = 12;
+
+            double area;
+
+            radio = radio * radio;
+
+            area = pi * radio;
+
+            Console.WriteLine("El area del circulo es");
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ tarea chidoris/tarea chida/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tarea_chida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double apotema = 5.2;
+
+            int lado = 6;
+
+            int perimetro;
+
+            double area;
+
+            perimetro = lado + lado + lado + lado + lado + lado;
+
+
+
+            area = perimetro * apotema / 2;
+
+            Console.WriteLine("el resultado es");
+            Console.WriteLine(area);
+
+        }
+    }
+}
