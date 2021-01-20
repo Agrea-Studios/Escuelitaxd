c36993075c9842c1bd1081ca03ca2f1bf9231a1a
DresdenMZ <maurix212@gmail.com>
2021-01-19 19:03:02 -0600

    Cambio total
    Cree una nueva carpeta e incluí mis dos proyectos.

---
+++ Proyecto de Mauricio/Prueba/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ap = 5.2;
+            int L = 6;
+            double A;
+            int P;
+            P = L*6;
+            A = (P*ap)/2;
+            Console.WriteLine("El área del hexágono es:"+ A);
+
+        }
+    }
+}
