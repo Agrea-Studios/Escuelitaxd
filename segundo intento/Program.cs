970b94d3e2c9cadb648eeb4f62095ce9cd3b923a
ElCarlos97 <66186531+ElCarlos97@users.noreply.github.com>
2021-01-19 23:03:43 -0600

    practica pedorra
    xd

--- segundo intento/Program.cs
+++ segundo intento/Program.cs
@@ -6,6 +6,7 @@ namespace segundo_intento
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Console.WriteLine("escribe el radio requerido para que el area de un circulo sea 78.54");
 
             double radio;
@@ -31,6 +32,44 @@ namespace segundo_intento
                  }
 
            
+=======
+           
+            decimal x;
+
+            decimal pi = 3.1416m;
+
+            Console.WriteLine("ingresar el valor para que el area de un circulo sea 113 o 53.93");
+
+            x = Convert.ToDecimal(Console.ReadLine());
+
+            decimal area;
+
+            x = x * x;
+
+            area = x * pi;
+
+            area = decimal.Round(area, 2);
+
+            switch (area)
+            {
+                case 153.94m:
+                Console.WriteLine("en efecto, es 113.10");
+                break;
+
+                case 113.10m:
+                Console.WriteLine("no pos equisde");
+                break;
+
+                case 78.54m:
+                Console.WriteLine("al chile tu resultado es 78.54");
+                break;
+
+                default:
+                Console.WriteLine("es falsooooooooo");
+                break;
+
+            }
+>>>>>>> Stashed changes
         }
     }
 }
