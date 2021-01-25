ea3cd4c9dd866229dfca8ab033302ea07298425e
ShrkyChanona <62265673+ShrkyChanona@users.noreply.github.com>
2021-01-24 21:19:40 -0600

    numero aleatorio
    clase chingona

---
+++ Chanona/generar numero aleatorio/generar numero aleatorio/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace generar_numero_aleatorio
+{
+    public class Class1
+    {
+        //participantes
+       public string nombre;
+        public string nombre1;
+        public string nombre2;
+        //numeros de rifa
+       public int Nrifa;
+        public int Nrifa1;
+        public int Nrifa2;
+        //numero aleatorio
+        public int Nuga;
+
+        public void SacoPico(string nombre,string nombre1,string nombre2, int rifa,int rifa1,int rifa2, int NuGa)
+        {
+            Console.WriteLine("los nombres son {0}, {1}, {2} y los numeros son {3}, {4}, {5} pero el numero ganador fue el {6}", nombre,nombre1,nombre2,rifa,rifa1,rifa2,NuGa);
+        }
+
+    }
+}
